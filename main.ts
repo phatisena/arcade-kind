452335179e8c414ee93995f8451037de4b6325ae
Phatisena <naphatism@hotmail.com>
2025-02-21 10:35:45 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
 namespace MyMemberKind {
+
     let nid: number;
     
-    export function create() { if (!(nid)) { nid = 0; return nid }; nid++; return nid; }
+    export function create() {
+        if (!(nid)) {
+            nid = 0;
+            return nid
+        };
+        nid++;
+        return nid;
+    }
+
 }
 
-//%block="my kind" color="#dbb62e" icon="\u23"
+//%block="my kind"
+//%color="#dbb62e"
+//%icon="\u23"
 namespace MyKinds {
 
     //%shim=KIND_GET kindMemberName=myMember block="$kindID"
-    //%blockId=Kinds_kind_get
+    //%blockId=MyKinds_kind_get
     //%kindNamespace=MyMemberKind 
     //%kindPromptHint="e.g. enter your member here"
-    export function getKind(kindID: number) { return kindID }
+    export function getKind(kindID: number) {
+        return kindID
+    }
 
 }
