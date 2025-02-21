20e28a2db05c21e73d23fc89bc3a446d339e3c38
Phatisena <naphatism@hotmail.com>
2025-02-21 10:29:16 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+namespace MyMemberKind {
+    let nid: number;
+    
+    export function create() { if (!(nid)) { nid = 0; return nid }; nid++; return nid; }
+}
+
+//%block="my kind" color="#dbb62e" icon="\u23"
+namespace MyKinds {
+
+    //%shim=KIND_GET kindMemberName=myMember block="$kindID"
+    //%blockId=Kinds_kind_get
+    //%kindNamespace=MyMemberKind 
+    //%kindPromptHint="e.g. enter your member here"
+    export function getKind(kindID: number) { return kindID }
+
+}
